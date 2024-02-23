fc3868a9e648cefedbc245800c5b049a917a3837
Chintan Sheth <chsheth@gmail.com>
2024-02-22 21:32:15 -0500

    pdf

--- test.py
+++ test.py
@@ -85,7 +85,7 @@ def main():
     st.set_page_config("Chat PDF")
     st.header("Chat with PDF using Gemini💁")
 
-    user_question = st.text_input("Ask a question to the pdf files")
+    user_question = st.text_input("Ask a Question from the PDF Files")
 
     if user_question:
         user_input(user_question)
